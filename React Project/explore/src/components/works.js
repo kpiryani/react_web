177fa82dd8000b9e6b1e1df187c0cc5294e10e02
kpiryani10 <piryanikrish@gmail.com>
2024-08-02 17:30:08 -0400

    fixing contact form

--- React Project/explore/src/components/works.js
+++ React Project/explore/src/components/works.js
@@ -6,12 +6,13 @@ import Portfolio2 from '../images/github_logo.png'
 const Works = () => {
   return (
     <section id='works'>
-      <h2 class="workstitle">My Portfolio</h2>
+      <h2 class="worksTitle">My Portfolio</h2>
       <span class="worksDesc"> The work I have done is a combination of internships and schoolwork.</span>
       <div class="worksImgs">
         <img src={Portfolio1} alt="" class="worksImg" />
         <img src={Portfolio2} alt="" class="worksImg" />
       </div>
+      <button class="worksBtn">See More</button>
     </section>
   );
 }
