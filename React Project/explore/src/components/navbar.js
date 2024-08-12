f2441b947acaf366054dc6933b63802c93b8f656
kpiryani10 <piryanikrish@gmail.com>
2024-08-12 12:34:55 -0400

    headers still corrupting

--- React Project/explore/src/components/navbar.js
+++ React Project/explore/src/components/navbar.js
@@ -9,9 +9,9 @@ const Navbar = () => {
     <nav className="navbar">
       <img src={logo} alt="Logo" className='logo'/>
       <div className="desktopMenu">
-        <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} class="desktopMenuListItem">Home</Link>
+        <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-200} duration={500} class="desktopMenuListItem">Home</Link>
         <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} class="desktopMenuListItem">About Me</Link>
-        <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500} class="desktopMenuListItem">Portfolio</Link>
+        <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} class="desktopMenuListItem">Portfolio</Link>
         <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} class="desktopMenuListItem">Experience</Link>
       </div>
     <button className="desktopMenuBtn" onClick={() => {
