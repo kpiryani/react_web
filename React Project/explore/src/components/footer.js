868478df1c1aab70dfe7cd1bfc0b09bb6bb5efe0
kopiryan <kopiryan@ncsu.edu>
2025-01-15 20:22:51 -0500

    updated footer links

--- React Project/explore/src/components/footer.js
+++ React Project/explore/src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <div>Copyright &#169; 2025 Krish Piryani. All rights reserved.</div>
       <br />
       <div>
-        Coded in <a className="vscode-link" href="#your-vscode-link">VS Code</a> with <a className="react-link" href="#your-react-link">React.js</a> and deployed with <a className="github-link" href="#your-github-link">Github</a>.
+        Coded in <a className="vscode-link" href="https://code.visualstudio.com/">VS Code</a> with <a className="react-link" href="#your-react-link">React.js</a> and deployed with <a className="github-link" href="#your-github-link">Github</a>.
       </div>
     </footer>
   );
